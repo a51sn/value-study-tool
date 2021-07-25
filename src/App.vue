9eca056ecd164d370f175bc7a1494cc3019d0851
Ashley Sun <37150737+a51sn@users.noreply.github.com>
2021-07-24 22:45:07 -0400

    added master & canvas components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container-fluid">
+      <Master/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Master from './pages/Master';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Master
   }
 }
 </script>
